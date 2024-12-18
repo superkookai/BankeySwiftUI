9a6e4f5f6e95af25ebea4be88b64fcfd2c9dfc74
Weerawut <weerawut@live.com>
2024-12-18 12:32:50 +0700

    Initial Commit

---
+++ BankeySwiftUI/BankeySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankeySwiftUIApp.swift
+//  BankeySwiftUI
+//
+//  Created by Weerawut Chaiyasomboon on 18/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BankeySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
